882c7515fbc82660aeea66eee3a36f1561241291
= <=>
2019-11-17 13:45:30 -0900

    passes all test

--- project7/treesort.h
+++ project7/treesort.h
@@ -21,6 +21,12 @@
 // for std::unique_ptr, std::make_unique
 
 
+// treeNode
+// node for binary seach tree
+// Requirements on Types:
+//     ValueType must be copyable
+// Exception safety guarantee:
+//     strong
 template<typename ValueType>
 struct treeNode
 {
@@ -28,9 +34,8 @@ struct treeNode
     std::unique_ptr<treeNode<ValueType>> lessChildTree;
     ValueType data;
     
-    
     treeNode(const ValueType & data):
-    data(data)
+    data(data) //may throw, let propigate out
     {}
 
     ~treeNode() = default;
@@ -45,48 +50,88 @@ struct treeNode
 
 };
 
-
+// insertTree
+// inserts given data item into tree, equal items are stored as lessthan to maintain stability when read back out
+// Requirements on Types:
+//     ValueType must be copyable
+// Exception safety guarantee:
+//     strong
 template<typename ValueType>
 void insertTree(std::unique_ptr<treeNode<ValueType>> & tree,const ValueType & dataToInsert) {
     if (!tree) {
-        tree = std::make_unique<treeNode<ValueType>>(dataToInsert);
+        tree = std::make_unique<treeNode<ValueType>>(dataToInsert); //may throw when calling ctor, let propigate out
         return;
     }
-    if (tree ->data < dataToInsert) {
-        insertTree(tree -> greaterChildTree, dataToInsert);
+    if (dataToInsert < tree -> data) {
+        insertTree(tree -> lessChildTree, dataToInsert);
     }
     else {
-        insertTree(tree -> lessChildTree, dataToInsert);
+        insertTree(tree -> greaterChildTree, dataToInsert);
     }
 }
 
-// treesort
-// Sort a given range using Treesort.
+
+// traverseTree
+// passed a function, calls it on every item in tree in order (lessthan order)
 // Pre:
-//     ???
+//     function must take 1 parameter, a location to copy to, 
+//     function must handle updating location between calls
 // Requirements on Types:
-//     must be copyable
+//     ValueType must be copyable
 //     
 // Exception safety guarantee:
-//     ???
+//     strong
+template<typename ValueType, typename func>
+void traverseTree(std::unique_ptr<treeNode<ValueType>> & head, func & function) {
+    if (head -> lessChildTree) {
+        traverseTree(head -> lessChildTree, function);
+    }
+    function(head -> data);
+
+    if (head -> greaterChildTree) {
+        traverseTree(head -> greaterChildTree, function);
+    }
+}
+
+
+// treesort
+// Sort a given range using Treesort.
+
+// Requirements on Types:
+//     ValueType must be copyable
+//     
+// Exception safety guarantee:
+//     strong
 template<typename FDIter>
 void treesort(FDIter first, FDIter last)
 {
     // ValType is the type that FDIter points to
     using ValueType = typename std::iterator_traits<FDIter>::value_type;
 
+    //don't sort an empty container
+    if (first == last){
+        return;
+    }
+
     std::unique_ptr<treeNode<ValueType>> head;
+
+    //iterate through container and add each item to tree
     for (FDIter it = first; it != last; ++it) {
         insertTree(head, *it);
     }
 
+    //copy out is a lamda function that is passed to traverse tree to copy data back into container,
+    FDIter it = first;
+    auto copyOut = [&] (ValueType & data) {
+            *it = data; //copy data
+            if (it != last) {
+                ++it; //satisfy precondition of traverse tree "function must handle updating location between calls"
+            }
+        };
+
+    //copy data back to original container
+    traverseTree(head, copyOut);
     
-    // THE FOLLOWING IS DUMMY CODE. IT WILL PASS ALL TESTS, BUT IT DOES
-    // NOT MEET THE REQUIREMENTS OF THE PROJECT.
-    std::vector<ValueType> buff(std::distance(first, last));
-    std::move(first, last, buff.begin());
-    std::stable_sort(buff.begin(), buff.end());
-    std::move(buff.begin(), buff.end(), first);
 }
 
 
